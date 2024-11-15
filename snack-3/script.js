04cc52166756bea660a64ae7f683ed512dc8a786
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2024-11-15 17:26:20 +0100

    feat: add snack-3

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,14 @@
 const numbers = [2, 8, 4, 7, 2, 87];
+console.log(numbers);
+
+const increasedNumbers = numbers.map((curNumber, i) => {
+    // console.log(curNumber);
+    increase = curNumber;
+    increase++
+    // console.log(increase);
+    return increase
+})
+console.log(increasedNumbers);
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
