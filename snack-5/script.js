4db8aecf53709f39b86ed24d855c6d55e21f0419
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2024-11-15 17:36:18 +0100

    feat: add snack-5

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,15 @@
 const nums = [2, 8, 4, 7, 12, 87];
+console.log(nums);
+
+const evenNums = nums.filter((curNum, i) => {
+    // console.log(curNum);
+    if (curNum % 2 === 0) {
+        return curNum;
+    };
+});
+console.log(evenNums);
+
+
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
