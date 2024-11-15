3dbefd379d33968f87073730afe0d08d257eaf8b
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2024-11-15 17:12:15 +0100

    feat: add snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,13 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
+// console.log(names);
+
+let result = "";
+names.forEach((curName, i) => {
+    // console.log(curName);
+    result += `${curName} `
+})
+console.log(result);
+
 
 // Stampa in console tutti i nomi
-// Risultato: 'Edoardo', 'Simone', 'Francesco'
+// Risultato: 'Edoardo', 'Simone', 'Francesco']
