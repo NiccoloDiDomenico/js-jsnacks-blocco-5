371e12101d76556ce0c8330cd2662ec62cf348c5
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2024-11-15 17:32:24 +0100

    feat: add snack-4

--- snack-4/script.js
+++ snack-4/script.js
@@ -4,6 +4,15 @@ const posts = [
   { author: 'Fabrizio', date: '12/9/2020', content: 'lorem ipsum...' },
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
+console.log(posts);
+
+const authorPosts = posts.map((curPost, i) => {
+  // console.log(curPost.author);
+  result = curPost.author
+  return result
+})
+console.log(authorPosts);
+
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
